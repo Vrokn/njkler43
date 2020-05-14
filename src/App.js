8d3ba68aa0e29b82c208c4f3e445cd28c328e245
Mateo <mgomezossa@gmail.com>
2020-05-13 19:10:20 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -5,17 +5,29 @@ import posts from './posts'
 // Modifica el componente App para implmentar la funcionalidad requerida
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      searchFilter: '',
+    }
+  }
+  updateSearch = (event) => {
+    event.preventDefault();
+    this.setState({ searchFilter: event.target.value })
+  }
   render() {
+    let filteredTerms = posts.filter((post) => {
+      return post.title.toLowerCase().indexOf(this.state.searchFilter.toLowerCase()) !== -1;
+    })
     return (
       <div>
         <div className="filter">
-          <input type="text" placeholder="Ingresa el término de búsqueda" />
+          <input value={this.state.searchFilter} type="text" placeholder="Ingresa el término de búsqueda" onChange={this.updateSearch.bind(this)} />
         </div>
         <ul>
-          <li>
-            <a href={posts[0].url}><img src={posts[0].image } /></a>
-            <p>{ posts[0].title }</p>
-          </li>
+          {filteredTerms.map((post) =>
+            <li><a href={post.url}><img src={post.image} /></a>
+              <p href={post.url}>{post.title}</p></li>)}
         </ul>
       </div>
     )
